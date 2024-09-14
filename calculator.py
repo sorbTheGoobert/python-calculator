75be1b904c7385e366d5fe72b38ce8118c6c972a
sorbonlikeidc <reeldob34@gmail.com>
2024-09-14 19:53:21 +0800

    Ok i havent tested the code yet and i probably wont for a while

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 def calculate() :
     result = 0
-    calculation = input()
+    # calculation = input()
+    calculation = "(((10    +              3) -           9) /    2)    -         (0.5)";
     match checkForError(calculation):
         case 1:
             raise Exception("ERROR: Incorrect parenthesis.")
@@ -9,6 +10,7 @@ def calculate() :
     calculation = removeWhiteSpaces(calculation)
     print(calculation)
     print("Errorless rn")
+    print(recursive(calculation))
 
 def checkForError(strVal) :
     stack = []
@@ -19,7 +21,7 @@ def checkForError(strVal) :
             case "(":
                 stack.append(0)
             case ")":
-                if not stack[-1] == 0:
+                if not stack[len(stack) - 1] == 0:
                     return 1
                 stack.pop()
         if not letter.isdigit() and letter not in allowedStrings:
@@ -33,27 +35,52 @@ def checkForError(strVal) :
 def removeWhiteSpaces(string) :
     return string.replace(" ", "")
 
-def calculateOperation() :
+def recursive(strval) :
+    substr = ""
+    for i in range(len(strval)):
+        if strval[i] == "(":
+            recursive(strVal[i+1:])
+        if strval[i] == ")":
+            break
+        substr += strval[i]
+
+    n1, op, n2 = extractFromStr(substr)
+    return calculateOperation(n1 ,op, n2);
+
+def extractFromStr(string):
+    n1, op, n2;
+    n1Done = False;
+    operatorSelection = "+-*/"
+    for letter in string:
+
+        if not n1Done:
+            if letter.isdigit() or letter == ".":
+                n1 += letter
+            elif operatorSelection.includes(letter):
+                op = letter
+                n1Done = True
+            continue
+
+        n2 += letter
+
+    n1 = float(n1)
+    n2 = float(n2)
+
+    return n1, op, n3
+
+def calculateOperation(a, op, b) :
     answer = 0
-    print("Insert num1.")
-    num1 = float(input())
-    print("Insert operator.")
-    operator = input()
-    print("Insert num2.")
-    num2 = float(input())
-    match operator:
+    match op:
         case "+":
-            answer = num1 + num2
+            answer = a + b
         case "-":
-            answer = num1 - num2
+            answer = a - b
         case "*":
-            answer = num1 * num2
+            answer = a * b
         case "/":
-            if num2 == 0 :
-                return "Please input correct value."
-            asnwer = num1 / num2
-        case _:
-            return "Please enter correct value."
-    return str(num1) + " " + operator + " " + str(num2) + " = " + str(answer)
+            if b == 0 :
+                raise Exception("ERROR: cannot divide by 0")
+            asnwer = a / b
+    return answer
 
 calculate()
